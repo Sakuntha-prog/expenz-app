c81e33cb886067a41097bab77d1f9e407bfd1156
Sakuntha sankalpa <sakunthasankalpa67@gmail.com>
2025-03-06 15:00:26 +0530

    onbord screen add to main app 2

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: OnbordScreen());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: OnbordScreen());
   }
 }
