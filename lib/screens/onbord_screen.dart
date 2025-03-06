175dbb29a6b32f67a5e87b0078c073e95042040e
Sakuntha sankalpa <sakunthasankalpa67@gmail.com>
2025-03-06 14:57:53 +0530

    onbord screen add to main app

---
+++ lib/screens/onbord_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnbordScreen extends StatefulWidget {
+  const OnbordScreen({super.key});
+
+  @override
+  State<OnbordScreen> createState() => _OnbordScreenState();
+}
+
+class _OnbordScreenState extends State<OnbordScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
